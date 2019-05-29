092d4c255d53a82139226216d96fca8158ac8494
Bishoy Farouk Tawfik <bftawfik@gmail.com>
2019-05-29 17:31:11 +0200

    end of the day 29 may

--- source/js/script.js
+++ source/js/script.js
@@ -1,15 +1,24 @@
 (function(){
     'use strict'
 
+    var piecesClickedCount = 0, piecesTotal = 7;
+
     document.addEventListener('DOMContentLoaded', function (event){
         var pieces = this.querySelectorAll('.piece');
         var piecesArray = Array.prototype.slice.call(pieces);
         for (var i = 0; i < piecesArray.length; i++) {
             piecesArray[i].addEventListener('click', function(event){
-                event.currentTarget.parentNode.appendChild(event.currentTarget);
-                event.currentTarget.classList.add('clicked');
-                // node.appendChild(textnode);  
-            });
+                if(!event.currentTarget.classList.contains('clicked')){
+                    event.currentTarget.parentNode.appendChild(event.currentTarget);
+                    event.currentTarget.classList.add('clicked');
+                    piecesClickedCount++;
+                    if(piecesClickedCount == piecesTotal){
+                        // event.currentTarget.classList.add('show');
+                        this.querySelector('.separator').classList.add('show');
+                        console.log(this.querySelector('.separator').classList)
+                    }
+                }
+            }.bind(this));
         }
         
     });
